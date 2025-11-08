a0d34abccc8dfd501531c7ef98125bf4b64adc06
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-11-08 17:25:01 +0800

    feat: added more fields to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,9 +20,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'office',
     ];
 
     /**
