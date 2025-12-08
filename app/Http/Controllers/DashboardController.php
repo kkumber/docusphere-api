8ef47e0ccde9e916e835c52d4b97b98dbf015461
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-12-08 16:11:25 +0800

    feat: sds dashboard data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
 use App\Models\Document;
+use App\Models\DocumentAssignment;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Carbon\Carbon;
@@ -22,13 +23,37 @@ class DashboardController extends Controller
     // 3. Total number of sds
     // 4. Total number of chief and staff
     // 5. Number of users created over time (area chart)
-    
+    if ($user->hasRole('admin')) {
+        $adminData = [
+            [
+                'title' => 'Total admin',
+                'data' => User::role('admin')->count(),
+            ],
+            [
+                'title' => 'Total records',
+                'data' => User::role('records')->count(),
+            ],
+            [
+                'title' => 'Total sds',
+                'data' => User::role('sds')->count(),
+            ],
+            [
+                'title' => 'Total chief and staff',
+                'data' => User::role('chief_and_staff')->count(),
+            ],
+            [
+                'title' => 'Users created over time',
+                'data' => User::whereBetween('created_at', [$startOfMonth, $endOfMonth])->count(),
+            ]
+            ];
+    }
+
 
     // Dashboard data for Records
     // 1. Total registered documents
     // 2. Documents archived
     // 3. Documents completed
-    // 4. Documents delayed
+    // 4. Documents pendings
     // 5. Documents received over time (area chart)
     
     if ($user->hasRole('records')) {
@@ -46,7 +71,7 @@ class DashboardController extends Controller
                 'data' =>  Document::where('assigned_to', $user->id)->count(),
             ],
             [
-                'title' => 'Total pending approvals',
+                'title' => 'Total pending documents',
                 'data' =>  Document::whereIn('status_id', [1, 6])->count(),
             ],
             [
@@ -59,11 +84,36 @@ class DashboardController extends Controller
     }
 
     // Dashboard data for SDS
-    // 1. Pending for signature / approval
-    // 2. Urgent / priority routings
-    // 3. Recently approved / signed
-    // 4. Returned to Records / Office
-    // 5. Approvals over time (area chart)
+    // 1. Total routed
+    // 2. Total completed
+    // 3. Total returned to records
+    // 4. total delayed
+    // 5. Document handled over time
+
+    if ($user->hasRole('sds')) {
+        $sdsData = [
+            [
+                'title' => 'Total routings',
+                'data' =>  Document::where('status_id', 7)->count(),
+            ],
+            [
+                'title' => 'Total completed tasks',
+                'data' =>  Document::where('status_id', 8)->count(),
+            ],
+            [
+                'title' => 'Total returned to records',
+                'data' => Document::join('document_assignment', 'document.id', '=', 'document_assignment.id')->where('status_id', 3)->count(),
+            ],
+            [
+                'title' => 'Total delayed tasks',
+                'data' =>  Document::where('status_id', 8)->count(),
+            ],
+            [
+                'title' => 'Document handled over time',
+                'data' => DocumentAssignment::where('')->whereBetween('created_at', [$startOfMonth, $endOfMonth])->count(),
+            ]
+        ];
+    }
 
     // Dashboard data for Chief
     // 1. Documents for review / endorsement
