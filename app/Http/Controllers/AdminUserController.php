fa3a83195a049b2862d19102aafc0b2dbd7b9a75
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-12-12 19:26:00 +0800

    feat: admin user controller update and delete

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,19 @@ class AdminUserController extends Controller
     {
         $users = User::join('model_has_roles', 'users.id', '=', 'model_has_roles.model_id')->all();
         return ApiResponse::success(data: $users);
+    }
+
+    public function update(User $user, RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user->update($validated);
+        return ApiResponse::success(data: $user);
+    }
+
+    public function destroy(User $user, Request $request)
+    {
+        $user->delete();
+        return ApiResponse::success(data: $user);
     }
 
 }
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules;
-use Spatie\Permission\Models\Role;
+
 
 class RegisteredUserController extends Controller
 {
@@ -20,28 +17,22 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(RegisterRequest $request): Response
     {
-        $request->validate([
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'office' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'role' => ['required', 'string', Rule::in(['records', 'sds', 'chief', 'staff'])],
-            'password' => ['required', Rules\Password::defaults()],
-        ]);
+        $validated = $request->validated();
 
         $user = User::create([
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'office' => $request->office,
-            'email' => $request->email,
-            'password' => Hash::make($request->string('password')),
+            'first_name' => $validated['first_name'],
+            'last_name' => $validated['last_name'],
+            'office' => $validated['office'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
         ]);
 
         event(new Registered($user));
+        
+        $user->assignRole($validated['role']);
 
-        $user->assignRole($request->role);
 
         return response()->noContent();
     }
