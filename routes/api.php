f1481f0fb587557f1cfd76e312399f1bcb1eb05e
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-06 15:59:18 +0800

    feat: registered document action controller functions

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,12 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::prefix('document-actions/document/{document}')->group(function () {
         Route::get('details', [DocumentActionController::class, 'details']);
+        Route::patch('acknowledge', [DocumentActionController::class, 'acknowledge']);
+        Route::patch('mark-as-done', [DocumentActionController::class, 'markAsDone']);
+        Route::patch('approve', [DocumentActionController::class, 'approve']);
+        Route::patch('sign', [DocumentActionController::class, 'sign']);
+        Route::patch('review', [DocumentActionController::class, 'review']);
+        Route::post('respond', [DocumentActionController::class, 'respond']);
     });
 
 });
