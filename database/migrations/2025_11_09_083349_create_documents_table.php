979a5daacd5e3a2f47b5229fc49a0fd010f32787
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-07 18:26:42 +0800

    fix: nullable due date for documents

--- database/migrations/2025_11_09_083349_create_documents_table.php
+++ database/migrations/2025_11_09_083349_create_documents_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('request_type', 50); // Must be checked in request for validation
             $table->foreignId('uploaded_by')->constrained('users')->onDelete('cascade');
             $table->foreignId('status_id')->constrained('statuses'); // Create a lookup table
-            $table->date('due_date');
+            $table->date('due_date')->nullable();
             $table->timestamps();
         });
     }
