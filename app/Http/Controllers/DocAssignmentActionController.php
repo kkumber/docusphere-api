21bc02b31b93ad2fb8a11985605c22a4a7a265c2
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-10 22:25:20 +0800

    feat: attachment get request

--- app/Http/Controllers/DocAssignmentActionController.php
+++ app/Http/Controllers/DocAssignmentActionController.php
@@ -7,6 +7,7 @@ use App\Models\DocAssignmentAction;
 use App\Models\Document;
 use App\Services\CloudinaryService;
 use Illuminate\Http\Request;
+use RuntimeException;
 
 class DocAssignmentActionController extends Controller
 {
@@ -14,7 +15,23 @@ class DocAssignmentActionController extends Controller
     {
         // Get all attachments on a document
         $documentAttachments = $document->documentFiles()->with(['user:id,first_name,last_name,email'])->where('is_primary', false)->get()->map(function ($file) use ($cloudinaryService) {
-            
+            $url = $cloudinaryService->generateSignedUrl($file->public_id);
+
+            if (!$url) {
+                throw new RuntimeException('Unable to generate signed URL');
+            };
+
+            return [
+                'id' => $file->id,
+                'name' => $file->file_name,
+                'url' => $url,
+                'user' => [
+                    'id' => $file->user->id,
+                    'first_name' => $file->user->first_name,
+                    'last_name' => $file->user->last_name,
+                    'email' => $file->user->email,
+                ]
+            ];
         });
         return ApiResponse::success(data: $documentAttachments);
     }
