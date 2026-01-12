677d59c1c3016c657d7d3da0b879497b2c49e380
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-12 21:35:58 +0800

    feat: document tracking index

--- app/Http/Controllers/DocumentTrackingController.php
+++ app/Http/Controllers/DocumentTrackingController.php
@@ -11,7 +11,32 @@ class DocumentTrackingController extends Controller
 {
     public function index(Document $document)
     {
-        $result = $document->documentTrackings()->with('sender', 'receiver')->latest()->get();
+    $result = $document->documentTrackings()
+        ->with(['sender', 'receiver'])
+        ->orderBy('created_at', 'desc')
+        ->get()
+        ->map(function ($tracking) {
+            return [
+                'id' => $tracking->id,
+                'status_id' => $tracking->status_id,
+                'remarks' => $tracking->remarks,
+                'created_at' => $tracking->created_at,
+
+                'from' => [
+                    'id' => $tracking->sender->id,
+                    'name' => $tracking->sender->first_name . ' ' . $tracking->sender->last_name,
+                    'role' => strtoupper($tracking->sender->role),
+                    'email' => $tracking->sender->email,
+                ],
+
+                'to' => [
+                    'id' => $tracking->receiver->id,
+                    'name' => $tracking->receiver->first_name . ' ' . $tracking->receiver->last_name,
+                    'role' => strtoupper($tracking->receiver->role),
+                    'email' => $tracking->receiver->email,
+                ],
+            ];
+        });
         return ApiResponse::success(data: $result);
     }
 }
