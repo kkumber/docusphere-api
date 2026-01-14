424bc9c79c9dea19b1ebe2899a4c2f388244a71c
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-14 21:41:52 +0800

    fix: alphabetical users

--- routes/api.php
+++ routes/api.php
@@ -27,11 +27,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Get All Users by Role
     Route::get('/users/roles', function () {
         $usersByRole = [
-            'admin' => User::role('admin')->get(['id', 'first_name', 'last_name', 'office']),
-            'records' => User::role('records')->get(['id', 'first_name', 'last_name', 'office']),
-            'sds' => User::role('sds')->get(['id', 'first_name', 'last_name', 'office']),
-            'chief' => User::role('chief')->get(['id', 'first_name', 'last_name', 'office']),
-            'staff' => User::role('staff')->get(['id', 'first_name', 'last_name', 'office']),
+            'admin' => User::role('admin')->orderBy('first_name')->get(['id', 'first_name', 'last_name', 'office']),
+            'records' => User::role('records')->orderBy('first_name')->get(['id', 'first_name', 'last_name', 'office']),
+            'sds' => User::role('sds')->orderBy('first_name')->get(['id', 'first_name', 'last_name', 'office']),
+            'chief' => User::role('chief')->orderBy('first_name')->get(['id', 'first_name', 'last_name', 'office']),
+            'staff' => User::role('staff')->orderBy('first_name')->get(['id', 'first_name', 'last_name', 'office']),
         ];
         
         return ApiResponse::success(data: $usersByRole);
