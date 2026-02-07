42dab9ac0276f5f332548d8aa647144a214c9353
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-02-07 20:55:20 +0800

    save

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -23,7 +23,7 @@ class DocumentService
     public function saveDocumentWithFileUpload(array $document, int $userId, string $folder, $file)
     {
         try {
-            DB::transaction(function () use ($document, $file, $folder, $userId) {
+            return DB::transaction(function () use ($document, $file, $folder, $userId) {
                 // upload file to cloudinary first
                 $uploadedFile = $this->cloudinaryService->uploadToCloudinary($file, $folder);
 
