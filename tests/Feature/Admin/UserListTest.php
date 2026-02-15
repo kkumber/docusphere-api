a5fb811cb6af7104f5a24bb6cef24271c695c4ae
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-02-15 15:40:29 +0800

    test

--- tests/Feature/Admin/UserListTest.php
+++ tests/Feature/Admin/UserListTest.php
@@ -1,1 +1,30 @@
-<?php ?>
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\getJson;
+
+describe('Admin User List', function () {
+    it('should return a list of users', function () {
+        $user = User::factory()->create();
+        $user->assignRole('admin');
+
+
+        $response = $this->actingAs($user)->getJson('/api/admin/users');
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'office',
+                    'role',
+                ]
+            ]
+        ]);
+    });
+});
+
+?>
