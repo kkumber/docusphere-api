9830e67af592067a559256c835f9d76fa03c70d6
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-03-10 16:26:53 +0800

    fix: improve start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-php artisan queue:work --daemon &
 
-# Cron scheduler in background (poor man's cron)
-while true; do
-    php artisan schedule:run
-    sleep 86400
-done &
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan queue:work --memory=128 --sleep=3 --tries=3 &
 
 php artisan serve --host=0.0.0.0 --port=$PORT
