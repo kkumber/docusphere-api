95db90aa897aa92c460f7c723cabb17bc92d72e7
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-05-26 13:43:50 +0800

    save

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
+
+echo "Installing dependencies..."
 composer install --no-dev --working-dir=/var/www/html
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Running seeders..."
-php artisan db:seed --force
+if (User::count() == 0); then
+    echo "Seeding database..."
+    php artisan db:seed --force
+fi
+
+echo "Caching config..."
+php artisan config:clear
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
