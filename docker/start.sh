bf95306cabb8156e2d974eb2d4ed25dc9490071f
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-05-27 23:51:52 +0800

    save

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-
 set -e
+
+cd /var/www/html
+
+# Cache config/routes
+php artisan config:cache
+php artisan route:cache
+
+# Run migrations
+php artisan migrate --force
 
 # Start PHP-FPM in background
 php-fpm -D
