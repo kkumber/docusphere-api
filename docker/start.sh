48f6ed6e364c7f749c9b42f27aaec111d9a09cfc
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-06-02 15:37:45 +0800

    prod: added ques

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,11 @@ php artisan route:clear
 php artisan config:cache
 php artisan route:cache
 
+echo "Starting Queue Worker..."
+php artisan queue:work --tries=3 --sleep=3 --timeout=90 --max-time=3600 &
+
+echo "Starting PHP-FPM..."
 php-fpm -D
 
+echo "Starting Nginx..."
 nginx -g "daemon off;"
